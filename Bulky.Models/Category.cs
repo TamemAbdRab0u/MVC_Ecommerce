90ef745bf36130439fcdcb0a104be426caf4810e
Tamem Abdrabou <tamemabdo21@gmail.com>
2025-09-13 07:45:13 +0300

    Feat: Seperated Models

---
+++ Bulky.Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Bulky.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Required*")]
+		[MaxLength(30)]
+        [DisplayName("Category Name")]
+		public string Name { get; set; }
+
+        [DisplayName("Display Order")]
+		[Range(1, 100,ErrorMessage = "Between 1:100")]
+		public int DisplayOrder { get; set; }
+
+    }
+}
