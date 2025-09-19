2eef5a9464ce4f9e9faf8f4c1bca12cd05201872
Tamem Abdrabou <tamemabdo21@gmail.com>
2025-09-19 09:29:35 +0300

    Feat: Company Model-Table

--- Bulky.DataAccess/Data/AppDbContext.cs
+++ Bulky.DataAccess/Data/AppDbContext.cs
@@ -10,6 +10,7 @@ namespace Bulky.DataAccess.Data
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
         public DbSet<Category> Categories { get; set; }
 		public DbSet<Product> Products { get; set; }
+        public DbSet<Company> Companies { get; set; }
 
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
 		{
