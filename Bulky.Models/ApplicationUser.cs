e5a5d52cc510338c62260fa29b724a11e8793673
Tamem Abdrabou <tamemabdo21@gmail.com>
2025-09-20 08:04:48 +0300

    Feat: Added Company Releation To ApplicationUser So We Can Select The Company On Register

--- Bulky.Models/ApplicationUser.cs
+++ Bulky.Models/ApplicationUser.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,5 +19,8 @@ namespace Bulky.Models
         public string? State { get; set; }
         public string? PostalCode { get; set; }
 
+        public int? CompanyId { get; set; }
+        [ForeignKey("CompanyId")]
+        public Company Company { get; set; }
     }
 }
