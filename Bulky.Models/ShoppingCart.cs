e3100039f22a3c1a76eb0ecf6b5f120456f2feb7
Tamem Abdrabou <tamemabdo21@gmail.com>
2025-09-21 08:44:41 +0300

    Feat: Added ShoopingCart Controller And Static Veiw And Added CartVM And Additional Price Property in ShoopingCartModel

--- Bulky.Models/ShoppingCart.cs
+++ Bulky.Models/ShoppingCart.cs
@@ -19,6 +19,8 @@ namespace Bulky.Models
         public string ApplicationUserId { get; set; }
         [ForeignKey("ApplicationUserId")]
         public ApplicationUser ApplicationUser { get; set; }
-   
-        }
+
+        [NotMapped]
+        public double Price { get; set; }
+    }
 }
---
+++ MVC - ProjectE -/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,59 @@
+﻿using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+using Bulky.Models.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Drawing.Text;
+using System.Security.Claims;
+
+namespace MVC___ProjectE__.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    [Authorize]
+    public class CartController : Controller
+    {
+        private readonly IUnitOfWork unitOfWork;
+        public CartVM CartVM { get; set; }
+        public CartController(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            var ClaimsIdentity = (ClaimsIdentity)User.Identity;
+            var UserId = ClaimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            CartVM = new()
+            {
+                Carts = unitOfWork.ShoppingCart.GetAll(x => x.ApplicationUserId == UserId, includeProperties: "Product"),
+                //OrderTotal = 0
+            };
+            foreach(var cart in CartVM.Carts)
+            {
+                cart.Price = GetPriceBasedOnQuantity(cart);
+                CartVM.OrderTotal += (cart.Price * cart.Count);
+            }
+            return View(CartVM);
+        }
+
+         private double GetPriceBasedOnQuantity(ShoppingCart cart)
+         {
+             if (cart.Count <= 50)
+             {
+                 return cart.Product.Price;
+             }
+             else
+             {
+                 if (cart.Count <= 100)
+                 {
+                     return cart.Product.Price50;
+                 }
+                 else
+                 {
+                     return cart.Product.Price100;
+                 }
+             }
+         }
+    }
+}
