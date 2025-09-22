e8e63007c54f0be471e1523322dd347db06ec05c
Tamem Abdrabou <tamemabdo21@gmail.com>
2025-09-22 09:13:09 +0300

    Feat: Added OrderHeader - OrderDetail Repository And UnitOfWork

---
+++ Bulky.DataAccess/Repository/Repository/OrderHeaderRepo.cs
@@ -0,0 +1,25 @@
+﻿using Bulky.DataAccess.Data;
+using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.DataAccess.Repository.Repository
+{
+    public class OrderHeaderRepo : Repository<OrderHeader>, IOrderHeaderRepo
+    {
+        private readonly AppDbContext context;
+        public OrderHeaderRepo(AppDbContext context) : base(context)
+        {
+            this.context = context;
+        }
+
+        public void Update(OrderHeader obj)
+        {
+            context.OrderHeaders.Update(obj);
+        }
+    }
+}
