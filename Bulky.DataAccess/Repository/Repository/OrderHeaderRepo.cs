6a189f966df630d106a1cf4c19665a92962fa080
Tamem Abdrabou <tamemabdo21@gmail.com>
2025-09-22 11:30:01 +0300

    Feat: Added Additional Updates In OrderHeader Repository (Stripe)

--- Bulky.DataAccess/Repository/Repository/OrderHeaderRepo.cs
+++ Bulky.DataAccess/Repository/Repository/OrderHeaderRepo.cs
@@ -21,5 +21,32 @@ namespace Bulky.DataAccess.Repository.Repository
         {
             context.OrderHeaders.Update(obj);
         }
+
+        public void UpdateStatus(int id, string orderStatus, string? paymentStatus = null)
+        {
+            var orderFromDb = context.OrderHeaders.FirstOrDefault(u => u.Id == id);
+            if (orderFromDb != null)
+            {
+                orderFromDb.OrderStatus = orderStatus;
+                if (!string.IsNullOrEmpty(paymentStatus))
+                {
+                    orderFromDb.PaymentStatus = paymentStatus;
+                }
+            }
+        }
+
+        public void UpdateStripePaymentID(int id, string sessionId, string paymentIntentId)
+        {
+            var orderFromDb = context.OrderHeaders.FirstOrDefault(u => u.Id == id);
+            if (!string.IsNullOrEmpty(sessionId))
+            {
+                orderFromDb.SessionId = sessionId;
+            }
+            if (!string.IsNullOrEmpty(paymentIntentId))
+            {
+                orderFromDb.PaymentIntentId = paymentIntentId;
+                orderFromDb.PaymentDate = DateTime.Now;
+            }
+        }
     }
 }
